f9367e7a9da5dd2ec382e4d81341aa76438a95a7
AlejandroFuentes14 <jfuentes0@ucol.mx>
2023-10-26 20:40:22 -0600

    PracticaR2

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Scene, Router, Actions } from 'react-native-router-flux';
+import LoginView from './src/LoginView';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const scenes = Actions.create(
+  <Scene key="root">
+    <Scene key="login" component={LoginView} hideNavBar />
+  </Scene>
+);
+
+export default class App extends Component {
+  render() {
+    return <Router scenes={scenes} />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
